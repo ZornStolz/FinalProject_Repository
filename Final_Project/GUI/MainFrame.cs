bc8c4003886df455e269f96a9eef9a7936ac95fe
Oscar Ivan Riascos Guevara <1006188503@academico.icesi.edu.co>
2020-03-09 16:54:05 -0500

    Se creo el diseño de la interfaz

--- Final_Project/GUI/MainFrame.cs
+++ Final_Project/GUI/MainFrame.cs
@@ -16,5 +16,39 @@ namespace GUI
         {
             InitializeComponent();
         }
+
+        private void MainFrame_Load(object sender, EventArgs e)
+        {
+            dtGrid.Columns.Add("Fecha", "Fecha");
+            dtGrid.Columns.Add("Autoridad Ambiental", "Autoridad Ambiental");
+            dtGrid.Columns.Add("Nombre de la estación", "Nombre de la estación");
+            dtGrid.Columns.Add("Tecnología", "Tecnología");
+            dtGrid.Columns.Add("Latitud", "Latitud");
+            dtGrid.Columns.Add("Longitud", "Longitud");
+            dtGrid.Columns.Add("Tipo de Dato", "Tipo de Dato");
+            dtGrid.Columns.Add("Código del departamento	", "Código del departamento	");
+            dtGrid.Columns.Add("Departamento", "Departamento");
+            dtGrid.Columns.Add("Código del municipio", "Código del municipio");
+            dtGrid.Columns.Add("Nombre del municipio", "Nombre del municipio");
+            dtGrid.Columns.Add("Tipo de estación", "Tipo de estación");
+            dtGrid.Columns.Add("Tipo de Dato", "Tipo de Dato");
+            dtGrid.Columns.Add("Tiempo de exposición", "Tiempo de exposición");
+            dtGrid.Columns.Add("Variable", "Variable");
+            dtGrid.Columns.Add("Unidades", "Unidades");
+            dtGrid.Columns.Add("Concentración", "Concentración");
+            dtGrid.Columns.Add("Nueva columna georreferenciada", "Nueva columna georreferenciada");
+
+        }
+
+        private void dtGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void btFilter_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
