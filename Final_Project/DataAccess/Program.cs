b06762e3c3b43d229077d04a51aaf7765c0af5be
ZornStolz <juandaveru@gmail.com>
2020-03-09 15:54:28 -0500

    cambiado el metodo de lectura de datos

--- Final_Project/DataAccess/Program.cs
+++ Final_Project/DataAccess/Program.cs
@@ -5,36 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using SODA;
-using SODA.Models;
-using SODA.Utilities;
 using Json.Net;
+using System.Net;
+using System.IO;
 
 namespace DataAccess
 {
     class Program
     {
+        /*
+         * source data to be consulted
+         */
+        public static String URL = "https://www.datos.gov.co/resource/ysq6-ri4e.json?";
+
         static void Main(string[] args)
         {
-
-            var client = new SodaClient("https://www.datos.gov.co", "8naPxF3oQIYI1NiilJm2JgR3q");
-
-            // Get a reference to the resource itself
-            // The result (a Resouce object) is a generic type
-            // The type parameter represents the underlying rows of the resource
-            // and can be any JSON-serializable class
-
-            var dataset = client.GetResource("ysq6-ri4e.json?" +
-                "$select=variable&$group=variable");
-
-            // Resource objects read their own data
-            var rows = dataset.GetRows(limit: 5000);
-
-            Console.WriteLine("Got {0} results. Dumping first results:", rows.Count());
-
-            foreach (var keyValue in rows.First())
-            {
-                Console.WriteLine(keyValue);
-            }
+            readInfo(URL + "variable=CO&$limit=5");
 
             /*
              * the first number in seconds
@@ -42,5 +28,38 @@ namespace DataAccess
             Thread.Sleep(60 * (Int32)Math.Pow(10, 3));
 
         }
-    }
+
+        private static void readInfo(String query)
+        {
+            string result = "";
+            try
+            {
+                var url = query;
+                var client = new WebClient();
+                using (var stream = client.OpenRead(url))
+                using (var reader = new StreamReader(stream))
+                {
+                    String line = reader.ReadLine();
+                    int count = 0;
+                    while ((line = reader.ReadLine()) != null && count <= 10)
+                    {
+                        String[] args = line.Split(',');
+                        Console.WriteLine(args[count]);
+                        count++;
+                    }
+                    reader.Close();
+                    stream.Close();
+                }
+
+            }
+            catch (WebException e)
+            {
+                result = string.Format("F", e);
+                Console.WriteLine(result);
+            }
+
+        }
+
+
+    } 
 }
