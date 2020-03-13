38b9c8ceadf8aa2b6384ea2fa694f9fe702ecc2e
Oss21 <osvan202@gmail.com>
2020-03-13 13:15:01 -0500

    Se termino parcialmente la interfaz

--- Final_Project/GUI/MainFrame.Designer.cs
+++ Final_Project/GUI/MainFrame.Designer.cs
@@ -28,32 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbURL = new System.Windows.Forms.TextBox();
-            this.valueToFilter = new System.Windows.Forms.TextBox();
+            this.txURL = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.cbFilterBy = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.dtGrid = new System.Windows.Forms.DataGridView();
             this.btFilter = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.fLP = new System.Windows.Forms.FlowLayoutPanel();
+            this.btCreateNewFilter = new System.Windows.Forms.Button();
+            this.prueba = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dtGrid)).BeginInit();
             this.SuspendLayout();
             // 
-            // tbURL
+            // txURL
             // 
-            this.tbURL.Location = new System.Drawing.Point(1039, 81);
-            this.tbURL.Name = "tbURL";
-            this.tbURL.Size = new System.Drawing.Size(100, 20);
-            this.tbURL.TabIndex = 0;
-            // 
-            // valueToFilter
-            // 
-            this.valueToFilter.Location = new System.Drawing.Point(1266, 139);
-            this.valueToFilter.Name = "valueToFilter";
-            this.valueToFilter.Size = new System.Drawing.Size(100, 20);
-            this.valueToFilter.TabIndex = 1;
+            this.txURL.Location = new System.Drawing.Point(1039, 81);
+            this.txURL.Name = "txURL";
+            this.txURL.Size = new System.Drawing.Size(100, 20);
+            this.txURL.TabIndex = 0;
             // 
             // label1
             // 
@@ -64,44 +59,18 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "URL";
             // 
-            // cbFilterBy
-            // 
-            this.cbFilterBy.FormattingEnabled = true;
-            this.cbFilterBy.Location = new System.Drawing.Point(1039, 139);
-            this.cbFilterBy.Name = "cbFilterBy";
-            this.cbFilterBy.Size = new System.Drawing.Size(121, 21);
-            this.cbFilterBy.TabIndex = 5;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(980, 142);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Filtrar por:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1195, 142);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Valor a filtrar :";
-            // 
             // dtGrid
             // 
             this.dtGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dtGrid.Location = new System.Drawing.Point(12, 12);
             this.dtGrid.Name = "dtGrid";
-            this.dtGrid.Size = new System.Drawing.Size(940, 643);
+            this.dtGrid.Size = new System.Drawing.Size(888, 643);
             this.dtGrid.TabIndex = 8;
             this.dtGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtGrid_CellContentClick);
             // 
             // btFilter
             // 
-            this.btFilter.Location = new System.Drawing.Point(1146, 193);
+            this.btFilter.Location = new System.Drawing.Point(1039, 508);
             this.btFilter.Name = "btFilter";
             this.btFilter.Size = new System.Drawing.Size(75, 23);
             this.btFilter.TabIndex = 9;
@@ -127,21 +96,74 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(308, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(153, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // fLP
+            // 
+            this.fLP.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fLP.AutoScroll = true;
+            this.fLP.Location = new System.Drawing.Point(906, 175);
+            this.fLP.Margin = new System.Windows.Forms.Padding(4);
+            this.fLP.Name = "fLP";
+            this.fLP.Size = new System.Drawing.Size(458, 326);
+            this.fLP.TabIndex = 13;
+            // 
+            // btCreateNewFilter
+            // 
+            this.btCreateNewFilter.Location = new System.Drawing.Point(1028, 133);
+            this.btCreateNewFilter.Name = "btCreateNewFilter";
+            this.btCreateNewFilter.Size = new System.Drawing.Size(211, 23);
+            this.btCreateNewFilter.TabIndex = 14;
+            this.btCreateNewFilter.Text = "Crear nuevo filtro";
+            this.btCreateNewFilter.UseVisualStyleBackColor = true;
+            this.btCreateNewFilter.Click += new System.EventHandler(this.btCreateNewFilter_Click);
+            // 
+            // prueba
+            // 
+            this.prueba.AutoSize = true;
+            this.prueba.Location = new System.Drawing.Point(1054, 579);
+            this.prueba.Name = "prueba";
+            this.prueba.Size = new System.Drawing.Size(35, 13);
+            this.prueba.TabIndex = 15;
+            this.prueba.Text = "label2";
+            // 
             // MainFrame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1582, 759);
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.prueba);
+            this.Controls.Add(this.btCreateNewFilter);
+            this.Controls.Add(this.fLP);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.btFilter);
             this.Controls.Add(this.dtGrid);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.cbFilterBy);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.valueToFilter);
-            this.Controls.Add(this.tbURL);
+            this.Controls.Add(this.btFilter);
+            this.Controls.Add(this.txURL);
             this.Name = "MainFrame";
             this.Text = "MainFrame";
             this.Load += new System.EventHandler(this.MainFrame_Load);
@@ -153,16 +175,18 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox tbURL;
-        private System.Windows.Forms.TextBox valueToFilter;
+        private System.Windows.Forms.TextBox txURL;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox cbFilterBy;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DataGridView dtGrid;
         private System.Windows.Forms.Button btFilter;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FlowLayoutPanel fLP;
+        private System.Windows.Forms.Button btCreateNewFilter;
+        private System.Windows.Forms.Label prueba;
     }
 }
 
--- Final_Project/GUI/MainFrame.cs
+++ Final_Project/GUI/MainFrame.cs
@@ -12,9 +12,21 @@ namespace GUI
 {
     public partial class MainFrame : Form
     {
+        /// <summary>
+        /// Count the click number of the button
+        /// </summary>
+        private int count_click;
+
+        private Label lbFilterBy;
+        private ComboBox cbFilterBy;
+        private Label lbValueToFilter;
+        private TextBox txValueToFilter;
+        private Button btAdd;
+        private Button btClear;
         public MainFrame()
         {
             InitializeComponent();
+            count_click = 1;
         }
 
         private void MainFrame_Load(object sender, EventArgs e)
@@ -38,11 +50,22 @@ namespace GUI
             dtGrid.Columns.Add("Concentración", "Concentración");
             dtGrid.Columns.Add("Nueva columna georreferenciada", "Nueva columna georreferenciada");
 
+            HandlerButtonAddAndClear(sender,e);
+
+
+
+
+
+            /////////////////////////////////
+
+
+
+
         }
 
         private void dtGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-           
+
         }
 
         private void btFilter_Click(object sender, EventArgs e)
@@ -50,5 +73,171 @@ namespace GUI
 
         }
 
+        private void btCreateNewFilter_Click(object sender, EventArgs e)
+        {
+
+            lbFilterBy = new Label();
+
+            cbFilterBy = new ComboBox();
+
+            lbValueToFilter = new Label();
+
+            txValueToFilter = new TextBox();
+
+            btAdd = new Button();
+            btClear = new Button();
+
+            createControlsyToFilter(lbFilterBy, cbFilterBy, lbValueToFilter, txValueToFilter, btAdd, btClear);
+
+            count_click++;
+
+
+        }
+
+
+        private void clearFilter()
+        {
+
+
+        }
+
+
+
+
+        /// <summary>
+        /// This method allows you to create the controllers responsible for filtering.
+        /// </summary>
+        private void createControlsyToFilter(Label lbFilterBy, ComboBox cbFilterBy, Label lbValueToFilter, TextBox txValueToFilter, Button btAdd, Button btClear)
+        {
+
+            // 
+            // lbFilterBy
+            // 
+            lbFilterBy.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            lbFilterBy.AutoSize = true;
+            lbFilterBy.Location = new System.Drawing.Point(3, 0);
+            lbFilterBy.Name = "lbFilterBy";
+            lbFilterBy.Size = new System.Drawing.Size(53, 29);
+            lbFilterBy.TabIndex = 6;
+            lbFilterBy.Text = "Filtrar por:";
+            lbFilterBy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            lbFilterBy.Name = "lbFilterBy" + count_click.ToString();
+
+
+            // 
+            // cbFilterBy
+            // 
+            cbFilterBy.FormattingEnabled = true;
+            cbFilterBy.Location = new System.Drawing.Point(62, 3);
+            cbFilterBy.Name = "cbFilterBy";
+            cbFilterBy.Size = new System.Drawing.Size(100, 21);
+            cbFilterBy.TabIndex = 5;
+            cbFilterBy.Name = "cbFilter" + count_click.ToString();
+
+            // 
+            // lbvalueToFilter
+            // 
+            lbValueToFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            lbValueToFilter.AutoSize = true;
+            lbValueToFilter.Location = new System.Drawing.Point(168, 0);
+            lbValueToFilter.Name = "lbvalueToFilter";
+            lbValueToFilter.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            lbValueToFilter.Size = new System.Drawing.Size(71, 29);
+            lbValueToFilter.TabIndex = 7;
+            lbValueToFilter.Text = "Valor a filtrar :";
+            lbValueToFilter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            txValueToFilter.Name = "tbValueToFilter" + count_click.ToString();
+
+
+            // 
+            // valueToFilter
+            // 
+            txValueToFilter.Location = new System.Drawing.Point(245, 3);
+            txValueToFilter.Name = "valueToFilter";
+            txValueToFilter.Size = new System.Drawing.Size(100, 20);
+            txValueToFilter.TabIndex = 1;
+            txValueToFilter.Name = "txValueToFilter" + count_click.ToString();
+
+
+            // 
+            // btAdd
+            // 
+            btAdd.Location = new System.Drawing.Point(351, 3);
+            btAdd.Name = "btAdd";
+            btAdd.Size = new System.Drawing.Size(75, 23);
+            btAdd.TabIndex = 8;
+            btAdd.Text = "Agregar";
+            btAdd.UseVisualStyleBackColor = true;
+            btAdd.UseVisualStyleBackColor = true;
+            btAdd.Name = "btAdd" + count_click.ToString();
+            btAdd.Click += new EventHandler(HandlerButtonAddAndClear);
+
+            // 
+            // btClear
+            // 
+            btClear.Location = new System.Drawing.Point(432, 3);
+            btClear.Name = "btClear";
+            btClear.Size = new System.Drawing.Size(21, 23);
+            btClear.TabIndex = 9;
+            btClear.Text = "X";
+            btClear.UseVisualStyleBackColor = true;
+            btClear.Name = "btClear" + count_click.ToString();
+            btClear.Click += new EventHandler(HandlerButtonAddAndClear);
+
+
+            /// Add elements to control Flow panel
+            this.fLP.Controls.Add(lbFilterBy);
+            this.fLP.Controls.Add(cbFilterBy);
+            this.fLP.Controls.Add(lbValueToFilter);
+            this.fLP.Controls.Add(txValueToFilter);
+            this.fLP.Controls.Add(btAdd);
+            this.fLP.Controls.Add(btClear);
+        }
+
+
+
+
+
+        public void HandlerButtonAddAndClear(object sender, EventArgs e)
+        {
+            Console.WriteLine("ss");
+            int i = 1;
+            bool found = false;
+            if (btClear != null )
+            {
+                while (i <= count_click && !found)
+                {
+                    if (((Button)sender).Name == "btClear"+i.ToString())
+                    {
+
+                        foreach (TextBox thecontrol in this.Controls.OfType<TextBox>().Where((fLP) => fLP.Name.Contains("lbFilterBy" + i.ToString())))
+                        {
+                            fLP.Controls.Remove(thecontrol);
+                            MessageBox.Show("Se presiono el boton"+i.ToString());
+                        }
+                        if (lbFilterBy.Name == "lbFilterBy"+i.ToString())
+                        {
+                        }
+
+                        found = true;
+                    }
+                    i++;
+                }
+               prueba.Text = btClear.Name;
+            }         
+              
+
+        }
+            
+
+            
+           
+
+           
+        
     }
 }
