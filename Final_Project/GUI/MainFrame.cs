e94ac55d2bc9a69fc4cf6a200a61f5c1901391ff
Gabriel Cruz <gabrielcl45@gmail.com>
2020-05-18 17:30:31 -0500

    Implement time series and arima algorithms. Add Microsoft.Chart.Controls NuGet package to display charts

--- Final_Project/GUI/MainFrame.cs
+++ Final_Project/GUI/MainFrame.cs
@@ -14,6 +14,8 @@ using System.Windows.Forms;
 using SODA;
 using Color = System.Drawing.Color;
 using Pen = System.Drawing.Pen;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace GUI
 {
@@ -1094,6 +1096,35 @@ namespace GUI
             minLabel.Text = "3- " + gsc.Min();
             desvLabel.Text = "4- " + gsc.desvit();
             phLabel.Text = gsc.hProof();
+        }
+        private void Arima(){
+    DataAnalysis processor = new DataAnalysis();
+        }
+
+        private void TimeSeries(){
+                double[] sunspots =
+                {
+                    100.8, 81.6, 66.5, 34.8, 30.6, 7, 19.8, 92.5,
+                    154.4, 125.9, 84.8, 68.1, 38.5, 22.8, 10.2, 24.1, 82.9,
+                    132, 130.9, 118.1, 89.9, 66.6, 60, 46.9, 41, 21.3, 16,
+                    6.4, 4.1, 6.8, 14.5, 34, 45, 43.1, 47.5, 42.2, 28.1, 10.1,
+                    8.1, 2.5, 0, 1.4, 5, 12.2, 13.9, 35.4, 45.8, 41.1, 30.4,
+                    23.9, 15.7, 6.6, 4, 1.8, 8.5, 16.6, 36.3, 49.7, 62.5, 67,
+                    71, 47.8, 27.5, 8.5, 13.2, 56.9, 121.5, 138.3, 103.2,
+                    85.8, 63.2, 36.8, 24.2, 10.7, 15, 40.1, 61.5, 98.5, 124.3,
+                    95.9, 66.5, 64.5, 54.2, 39, 20.6, 6.7, 4.3, 22.8, 54.8,
+                    93.8, 95.7, 77.2, 59.1, 44, 47, 30.5, 16.3, 7.3, 37.3,
+                    73.9
+                };
+
+                Series series = new Series();
+                for (int i = 0; i < sunspots.Length; i++)
+                {
+                    DataPoint dataPoint = new DataPoint(i, sunspots[i]);
+                    series.Points.Add(dataPoint);
+                }
+
+                timeSeries.Series[0].Points.DataBindY(sunspots);
         }
     }
 }
